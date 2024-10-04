4918da6b679359109cb523f58f75a4cc0bc037a5
LizetteTrejo <lizette.trejo@hotmail.com>
2024-10-04 15:41:53 -0600

    feat: Se agrega el uso de dom

---
+++ introjs/ejerciciosjs/js06-dom/script.js
@@ -0,0 +1,70 @@
+console.log("JS05 - Operadores");
+
+
+/**
+  Cambiar el contenido de un elemento HTML por medio de su ID
+  
+  Las principales diferencias entre innerHTML y textContent son:
+  Contenido interpretado:
+     innerHTML: Inserta y retorna el contenido HTML, interpretando 
+      las etiquetas y creando elementos.
+     textContent: Inserta y retorna solo el texto, sin interpretar 
+      ni procesar etiquetas HTML.       
+   
+  Seguridad:
+     innerHTML: Es vulnerable a ataques XSS si no se valida 
+      adecuadamente el contenido.
+     textContent: Es más seguro, ya que trata todo el contenido 
+      como texto, sin ejecutar código HTML o scripts.
+  Rendimiento:
+    innerHTML: Puede ser más lento si el contenido HTML tiene que ser reanalizado o renderizado.
+    textContent: Es más rápido ya que solo maneja texto plano.
+ */
+
+
+const changeElementById = (idName, newMessage) => {
+ const refElement = document.getElementById(idName);
+ console.log(refElement);
+ refElement.innerHTML=newMessage;
+};
+changeElementById("title", "Es tan fetch la sesión");
+changeElementById("textoh2", "Why are you so obsessed with me?");
+
+const changeElementsByTagName = () =>{
+  // Obteniendo la referencia de los <li></li>
+  const listItems = document.getElementsByTagName("li");
+  console.log( listItems );
+  for (let index = 0; index < listItems.length; index++) {    
+    listItems[index].textContent = `Soy el índice ${index} `;
+  }
+}
+changeElementsByTagName();
+
+/**
+ * Modificar un elemento usando el selector universal
+ */
+const changeElementsByQuerySelector = () => {
+  // const element = document.querySelector( "img" );
+  const element = document.querySelector( "#logo" );
+  console.log( element.src ); //Leer la ruta de la imagen
+  console.log( element.alt ); //Leer el contenido de alt
+  element.src = "sus.jpg";
+  element.alt = "mean girls";
+}
+changeElementsByQuerySelector();
+
+// ------------------ Propiedades de visualización ---------------
+// display : none (quitar el elemento del DOM)
+// visibility: hidden (ocultar el elemento)
+const displayNone = () => {
+  const element = document.querySelector("#textoh2");
+  element.style.display = "none"; // quitar el elemento
+  // element.style.display = "inline"; // mostrar "inline", "block"
+}
+const hiddenElement = () => {
+  const element = document.querySelector("#second-list-item");
+  element.style.visibility = "hidden";
+  // element.style.visibility = "visible"; // mostrar el elemento
+}
+displayNone();
+hiddenElement();
