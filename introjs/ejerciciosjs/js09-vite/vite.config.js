a287e4909ff21e1a6196371161c5db1e46cd7814
LizetteTrejo <lizette.trejo@hotmail.com>
2024-10-10 14:38:26 -0600

    feat: se agrega el uso de vite

---
+++ introjs/ejerciciosjs/js09-vite/vite.config.js
@@ -0,0 +1,12 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        aboutUs: resolve(__dirname, 'src/pages/aboutUs/aboutUs.html'),
+      },
+    },
+  },
+})
